80fc6e510cdf8c30614de4b38eca7522f89bdd41
Daniel RB <daniel@danielrb.dev>
2022-10-13 21:25:38 -0400

    fix: typo

--- laclong/utils.go
+++ laclong/utils.go
@@ -8,6 +8,6 @@ import (
 
 func GetFileName(contestantId string) string {
 	fileName := fmt.Sprintf("%s_ticks.csv", strings.Replace(contestantId, "/", "_", -1))
-	fileName = filepath.Join("./", fileName)
+	fileName = filepath.Join("/data", fileName)
 	return fileName
 }
